7bf45e13e259b3d4acd454208a5e9f77fd358bc7
Crimson <128308156+Crystonix@users.noreply.github.com>
2024-04-15 19:49:40 +0200

    configs

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    // Base URL for the production build (optional, only needed if your site is hosted on a subdirectory)
+    base: '/unterwasser/',
+});
